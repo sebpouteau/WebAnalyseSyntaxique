c0709e50e36a37fdd534b300a9180c01f94b6718
Krazyxx <jimmy.gouraud@etu.u-bordeaux.fr>
2016-03-08 09:32:40 +0100

    modification du parcours en profondeur

--- projet/xml_builder.c
+++ projet/xml_builder.c
@@ -13,7 +13,7 @@ static void build_xml(tree t){
 
 static void attributes_to_xml(attributes a){
   while(a != NULL){
-    printf("%s=\"%s\"", get_key(a), get_value(a));
+    printf(" %s=\"%s\"", get_key(a), get_value(a));
     a = get_next(a);
     if(a != NULL){
       printf(" ");
@@ -30,50 +30,39 @@ static void describe_word(tree t){
   
 }
 
-static void depth_search(tree t){
-  if(t != NULL){
-    if(get_tp(t) == _word){
-      printf("%s ", get_label(t));
-    } else {
-      printf("<%s ", get_label(t));
-      attributes_to_xml(get_attributes(t));
-      printf(">\n");
-   
-    }
-       if(get_daughters(t) != NULL){
-        depth_search(get_daughters(t));
-        printf("</%s>", get_label(t));
-      } else if(get_right(t) != NULL){
-        depth_search(get_right(t));
-      } else
-        return;
-  }    
-}
 
-static void tree_to_xml(tree t){
-  while(t != NULL){
-    if(get_label(t) != NULL){
-      if(get_tp(t) == _word){ //Si c'est pour def un sous arbre alors
-        printf("%s", get_label(t)); //On le balise
-        if(get_attributes(t) != NULL){
-          if(get_space(t)){
-            printf(" ");
-          }
-          attributes_to_xml(get_attributes(t));
-          printf(">");
-        }
-      } else { //Sinon
-        printf("<%s", get_label(t)); //C'est de type word donc du text
-      }
-      if(get_right(t) != NULL){
-        t = get_right(t);
-      }
-      else {
-        t = get_daughters(t);
-      }
-    }
+static void print_label(tree t){
+  printf("%s", get_label(t));
+  if (get_space(t)) {
+    printf(" ");
   }
 }
+
+static void depth_search(tree t){
+  if(t == NULL)
+    return;
+  
+  if(get_tp(t) == _word) {
+    print_label(t);
+  } else {
+    printf("<");
+    print_label(t);
+    if (get_nullary(t)) {
+      printf("/>");
+    } else {
+      attributes_to_xml(get_attributes(t));
+      printf(">");
+      depth_search(get_daughters(t));
+      printf("</%s>", get_label(t));
+    }
+  }
+  if(get_right(t) != NULL){
+    depth_search(get_right(t));
+  }
+}
+
+
+
 
 int main(void){
   attributes a = create_attributes("href", "www.google.fr", NULL);
@@ -81,12 +70,16 @@ int main(void){
   attributes a2 = create_attributes("bold", "false", NULL);
   set_next(a, a1);
   set_next(a1, a2);
-  //attributes_to_xml(a);
 
-  tree t4 = create_tree("a", true, false, _tree, a, NULL, NULL);
-  tree t3 = create_tree("Bonjour", true, false, _word, NULL, NULL, NULL);
-  tree t2 = create_tree("Hello", true, false, _word, NULL, t4, t3);
-  tree t = create_tree("div", false, true, _tree, a, t2, NULL);
+  tree t = NULL;
+  tree t2 = NULL;
+  tree t3 = NULL;
+  tree t4 = NULL;
+  t3 = create_tree("Bonjour", true, false, _word, NULL, NULL, NULL);
+  t4 = create_tree("a", false, false, _tree, NULL, t3, NULL);
+  t2 = create_tree("Hello", true, true, _word, NULL, NULL, t4);
+  t = create_tree("div", false, false, _tree, NULL, t2, NULL);
+
 
   depth_search(t);
   /*
