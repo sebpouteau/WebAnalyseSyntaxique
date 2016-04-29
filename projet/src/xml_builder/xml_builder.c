509a492ac89355fb657b9171be390da05e779c3d
Krazyxx <jimmy.gouraud@etu.u-bordeaux.fr>
2016-04-29 14:07:35 +0200

    xml_builder modifications

--- projet/src/xml_builder/xml_builder.c
+++ projet/src/xml_builder/xml_builder.c
@@ -15,20 +15,10 @@ void xml_ast (FILE* fd, struct ast* a, int depth_indent){
     
   switch(a->type){
     case INTEGER:
-      fprintf(fd,  " %d ", a->node->num);
-      break;
-    case BINOP:
-      break;
-    case UNARYOP:
-      break;
-    case VAR:      
+      fprintf(fd,  "%d", a->node->num);
       break;
     case WORD:
-      fprintf(fd, " %s ", a->node->str);
-      break;
-    case IMPORT:
-      break;
-    case APP:
+      fprintf(fd, "%s", a->node->str);
       break;
     case TREE:
       xml_tree(fd, a->node->tree, depth_indent);
@@ -36,14 +26,15 @@ void xml_ast (FILE* fd, struct ast* a, int depth_indent){
     case FOREST:
       xml_forest(fd, a->node->forest, depth_indent);
       break;
+    case BINOP:
+    case UNARYOP:
+    case VAR:      
+    case IMPORT:
+    case APP:
     case FUN:
-      break;
     case MATCH:
-      break;
     case COND:
-      break;
     case DECLREC:
-      break;
     default:
       break;
   }
@@ -75,12 +66,22 @@ void xml_tree(FILE* fd, struct tree* t, int depth_indent){
     return;
   }
   
+  if (strcmp(t->label, "word") == 0){
+    xml_ast(fd, t->daughters, depth_indent+1);
+    if (t->space){
+      fprintf(fd, " ", t->label);
+    }
+    return;
+  }
+  
   xml_indent(fd, depth_indent);
   fprintf(fd, "<%s", t->label);
   xml_attributes(fd, t->attributes, depth_indent);
   fprintf(fd, ">\n");
-  
+
   xml_ast(fd, t->daughters, depth_indent+1);
+  
+  fprintf(fd, "\n", t->label);
   xml_indent(fd, depth_indent);
   fprintf(fd, "</%s>\n", t->label);
 }
@@ -104,61 +105,8 @@ void build_xml(char * file,struct ast* a){
     exit(EXIT_FAILURE);
   }
   
-  fprintf(fd, "<!DOCTYPE html>");
-  
+  fprintf(fd, "<!DOCTYPE html>\n");
   xml_ast(fd, a, 0);
   
   fclose(fd);
 }
-
-
-
-// =================================================
-/*
-
-void print_label(tree t){
-  printf("%s", tree_get_label(t));
-  if (tree_get_space(t)) {
-    printf(" ");
-  }
-}
-
-bool is_word(tree t){
-  if (t == NULL || tree_get_tp(t) != _word)
-    return false;
-  return true;
-}
-
-void depth_search(tree t, int cpt){
-  if(t == NULL)
-    return;
-  
-  if(tree_get_tp(t) == _word) {
-    print_label(t);
-    if (!is_word(tree_get_right(t)))
-      printf("\n");
-  }
-  else {
-    indent_search(cpt);
-    printf("<");
-    print_label(t);
-    if (tree_get_nullary(t))
-    {
-      printf("/>\n");
-    }
-    else
-    {
-      attributes_to_xml(tree_get_attributes(t));
-      printf(">\n");
-      if (is_word(tree_get_daughters(t)))
-        indent_search(cpt+1);
-      depth_search(tree_get_daughters(t), cpt+1);
-      indent_search(cpt);
-      
-      printf("</%s>\n", tree_get_label(t));
-    }
-    if (is_word(tree_get_right(t)))
-      indent_search(cpt);
-  }
-  depth_search(tree_get_right(t), cpt);
-}*/
